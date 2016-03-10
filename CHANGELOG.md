09e589067a8d060998210dc849607d332f88e51e
TJ Egan <twegan@us.ibm.com>
2016-03-09 22:52:02 -0600

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## Feb 2, 2016
+- Change engine to Node v4.2
+- Change express version to 4.13
---
+++ app.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var request = require('request');
+var https = require('https');
+var cfenv = require('cfenv');
+
+
+
+// create a new express ser ver
+var app = express();
+var http = require('http')
+var server = http.Server(app);
+var io = require('socket.io')(server);
+server.listen(process.env.PORT || 3000)
+
+
+// Handlebars initialization
+var handlebars = require('express-handlebars').create( { defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+
+// Default route
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+io.on('connection', function (socket) {
+    console.log('User connected!');
+
+    socket.on('weatherSearch', function(query) {
+        var url = 'https://autocomplete.wunderground.com/aq?query=' + query;
+        var searchOutput = getWeatherQuery(url, function(results) {
+            var searchOutput = handleResults(results);
+            io.emit('weatherSearch', searchOutput)
+        })
+    })
+
+    socket.on('weatherClick', function(query) {
+        var url = 'http://api.wunderground.com/api/30fd7a559cd49cb5/history_20160309' + query + '.json'
+        var weatherOutput = getWeather(url, function(results) {
+            var almanac = handleWeather(results);
+            var max = almanac[0],
+                min = almanac[1]
+
+            io.emit('weatherClick', max, min)
+        })
+    })
+})
+
+function weatherGuess(url, callback) {
+
+}
+
+function handleResults(results) {
+    var output = '<ul class="search-results__list">'
+
+    for( var i = 0; i < results.length; i++) {
+        output += '<li><a href="" data-attr="' +
+        results[i].l +
+        '" class="search__result">' +
+        results[i].name +
+        '</a></li>'
+    }
+    output += '</ul>'
+    return output
+}
+
+function getWeatherQuery(url, callback) {
+    var options = {
+        url: url,
+        method : 'GET'
+    };
+    var res = '';
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res = JSON.parse(body).RESULTS;
+        }
+        else {
+            res = 'Not Found';
+        }
+        callback(res);
+    });
+}
+
+function getWeather(url, callback) {
+    var options = {
+        url: url,
+        method : 'GET'
+    };
+    var res = '';
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res = JSON.parse(body).history.dailysummary;
+        }
+        else {
+            res = 'Not Found';
+        }
+        callback(res);
+    });
+}
+
+function handleWeather(results) {
+    var max = results[0].maxtempi
+    var min = results[0].mintempi
+    var arr = [];
+
+    arr.push(max, min);
+    return arr;
+}
+
+
+
+module.exports = app
